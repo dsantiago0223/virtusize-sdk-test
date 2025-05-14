ffb19e895812687d1f01ebe3d6014ee47dfa61ae
Dave Santiago <dsantiago@hungryfriend.com>
2025-05-14 09:02:50 +0800

    update

--- VirtusizeSDK/build.gradle.kts
+++ VirtusizeSDK/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 val mGroupId = "com.virtusize"
 val mArtifactId = "sdk"
 val mVersionCode = 1
-val mVersionName = "1.0.8"
+val mVersionName = "1.0.9"
 val mLibraryName = "VirtusizeSDK"
 val mLibraryDescription = "Virtusize SDK test library"
 
